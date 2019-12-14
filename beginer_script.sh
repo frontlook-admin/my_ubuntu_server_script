c054d798acd563d0a2c01497254ccc3b6ea5df12
Frontlook Admin <frontlook.inc@gmail.com>
2019-12-14 15:09:46 +0000

    Update beginer_script.sh

--- beginer_script.sh
+++ beginer_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt update & apt upgrade -y
-apt install openssh
+apt install openssh-server
 apt install mysql-server
 cp mystartup.sh /etc/init.d/mystartup.sh
 cp custom_uwf.service /etc/systemd/system/custom_uwf.service
