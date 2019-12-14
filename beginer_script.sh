a85796bb5bdf9ef0ff868bc74df21dc1befba87c
Debanjan Chakraborty <52099846+frontlook-admin@users.noreply.github.com>
2019-12-14 11:48:20 +0530

    Update beginer_script.sh

--- beginer_script.sh
+++ beginer_script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+apt install openssh
+apt install mysql-server
 cp mystartup.sh /etc/init.d/mystartup.sh
 cp custom_uwf.service /etc/systemd/system/custom_uwf.service
 chmod +x /etc/init.d/mystartup.sh
